f0fc7a72062c923e558ca417fb7480621f101b3f
adriavc00 <adriavciscar@gmail.com>
2020-12-05 13:58:03 +0100

    Create first version FXMLMain, with make controller and set min size.

--- src/tutorias/Tutorias.java
+++ src/tutorias/Tutorias.java
@@ -24,6 +24,8 @@ public class Tutorias extends Application {
         Scene scene = new Scene(root);
 
         stage.setScene(scene);
+        stage.setMinHeight(420);
+        stage.setMinWidth(650);
         stage.show();
     }
 
