2ea11391ab4f1ca2ff9564bb46c2f5a775bf9193
adriavc00 <adriavciscar@gmail.com>
2020-12-08 11:07:14 +0100

    Feature addSubject and addStudent windows stablished

--- src/controllers/FXMLMainController.java
+++ src/controllers/FXMLMainController.java
@@ -5,9 +5,18 @@
  */
 package controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,6 +24,10 @@ import javafx.fxml.Initializable;
  * @author lipez
  */
 public class FXMLMainController implements Initializable {
+    @FXML
+    private MenuItem addSubjectItem;
+    @FXML
+    private MenuItem addStudentsItem;
 
     /**
      * Initializes the controller class.
@@ -22,6 +35,35 @@ public class FXMLMainController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    @FXML
+    private void addSubject(ActionEvent event) throws IOException {
+        FXMLLoader customLoader = new FXMLLoader(getClass().getResource("/views/FXMLAddSubject.fxml"));
+        Parent root = customLoader.load();
+
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setTitle("Añadir asignatura");
+        stage.setScene(scene);
+        stage.showAndWait();
+        FXMLaddSubjectController controller = customLoader.getController();
+        // TODO: Functionality
+    }
+
+    @FXML
+    private void addStudent(ActionEvent event) throws IOException {
+        FXMLLoader customLoader = new FXMLLoader(getClass().getResource("/views/FXMLAddSubject.fxml"));
+        Parent root = customLoader.load();
+
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setTitle("Añadir asignatura");
+        stage.setScene(scene);
+        stage.showAndWait();
+        FXMLaddSubjectController controller = customLoader.getController();
+    }
+
 }
