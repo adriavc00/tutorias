114d7528b2d9bbe8edb204d4b34b663185869acd
adriavc00 <adriavciscar@gmail.com>
2020-12-08 09:40:47 +0100

    Rename packages and GridTimeSlot.jar added to project

--- src/main/FXMLMainController.java
+++ src/main/FXMLMainController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Controllers;
+package main;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,7 +14,7 @@ import javafx.fxml.Initializable;
  *
  * @author lipez
  */
-public class FXMLaddSubjectController implements Initializable {
+public class FXMLMainController implements Initializable {
 
     /**
      * Initializes the controller class.
