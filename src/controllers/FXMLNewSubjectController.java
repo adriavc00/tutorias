4798c333d229d10de30b46fd920d6f368ea5813b
lipez <lipez@LAPTOP-C80I3VLN.home>
2020-12-13 18:45:55 +0100

    Correcting problems of FXMLadd controllers

---
+++ src/controllers/FXMLNewSubjectController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author lipez
+ */
+public class FXMLNewSubjectController implements Initializable {
+
+    @FXML
+    private Button cancelButton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void addSubject(ActionEvent event) {
+    }
+
+    @FXML
+    private void cancel(ActionEvent event) {
+        ((Stage) cancelButton.getScene().getWindow()).close();
+    }
+    
+}
